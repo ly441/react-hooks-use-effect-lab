8f6a155d4b096febc36feebdeced9f741ff01288
Lynn <koliilynn@gmail.com>
2025-04-18 12:45:41 +0300

    Add useEffect to manage countdown timer in Question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+ useEffect(() => {
+    // Set a timeout that triggers every second
+    const timer = setTimeout(() => {
+      setTimeRemaining((prev) => {
+        if (prev === 1) {
+          // Reset timer and call onAnswered when the timer hits 0
+          onAnswered(false);
+          return 10;
+        } else {
+          // Decrement the timer each second
+          return prev - 1;
+        }
+      });
+    }, 1000);
+
+    // Cleanup the timeout when the component unmounts
+    return () => clearTimeout(timer);
+  },[timeRemaining,onAnswered]);
+   
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
